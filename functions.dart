39e1d246a750d0d3004b218e18d70626ec8c75f3
Andruixxd31 <andruix93@gmail.com>
2020-08-27 13:28:20 -0500

    Language tour of dart

--- functions.dart
+++ functions.dart
@@ -1,17 +1,34 @@
+// Dart is a true object-oriented language, so even functions are objects
+// and have a type Function
+// functions can be assigned to variables or passed as arguments to other functions
+
 main() {
   var result = fibonacci(20);
-
+  var says = say("Andres", "Hola", "Cellphone");
+  print(says);
   print(size(10));
 }
 
+//Functions still work without type annotations
 int fibonacci(int n) {
   if (n == 0 || n == 1) return n;
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-var size = (int s) => s * 2; //Shorthand function
+//Shorthand function. For functions with one expression
+// The => expr syntax is a shorthand for { return expr; }
+var size = (int s) => s * 2;
 
 //Similiar to the Shorthand function
 int sizeN(int s) {
   return s * 2;
+}
+
+//The brackets specify and optional parameters
+String say(String from, String msg, [String device]) {
+  var result = '$from says $msg';
+  if (device != null) {
+    result = '$result with a $device';
+  }
+  return result;
 }
--- maps.dart
+++ maps.dart
@@ -1,8 +1,17 @@
 main() {
+  //dart infers that this is a Map<String, String>
   var gifts = {
     // Key:    Value
     'first': 'partridge',
     'second': 'turtledoves',
     'fifth': 'golden rings'
   };
+
+  //We can use the "new" keyword but it is optional
+  var nobleGases = new Map();
+  nobleGases[2] = "helium";
+  nobleGases[10] = 'neon';
+  nobleGases[18] = 'argon';
+
+  print(nobleGases[2]);
 }
